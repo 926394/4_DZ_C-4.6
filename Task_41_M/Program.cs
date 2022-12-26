05d5df46677c4144555f68de1808a7bdbc4f5015
Nikolay <meta1mang@mail.ru>
2022-12-26 19:41:19 +0300

    .\Task_41_M\

---
+++ Task_41_M/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+Console.Write("Введите количество чисел: ");
+int length = int.Parse(Console.ReadLine()!);
+
+int[] array = new int [length];
+
+void GetMassivNum(int[] coll) 
+{
+    for(int index = 0; index < coll.Length; index ++)
+    {
+        Console.Write($"Введите {index +1} число: ");
+        coll[index] = int.Parse(Console.ReadLine()!);
+    }
+}
+
+int TheSumNum(int[] coll)
+{
+    int count = 0;
+    for (int index = 0; index < coll.Length; index++)
+    {
+
+        if (coll[index] > 0)
+        {
+            count++; 
+        }
+
+    }
+    return count;
+}
+
+GetMassivNum(array); 
+Console.WriteLine(); 
+Console.WriteLine(TheSumNum(array)); 
